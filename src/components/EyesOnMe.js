45287b7b161e568556656e6722a16cad3d16b244
yego <yegokipkemoi254@gmail.com>
2022-09-23 00:13:28 +0300

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+
+import React from 'react'
+
+function EyesOnMe() {
+ 
+    function handleFocus(){
+        console.log('Good!');
+    }
+
+    function hadleBlur(){
+        console.log('Hey! Eyes on me!');
+    }
+
+  return (
+    <div>
+        <button onFocus={handleFocus} onBlur={hadleBlur}>
+            Eyes on me
+        </button>
+    </div>
+  )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
 function Keypad (){
+
+    function handleChange() {
+        console.log('Entering password...');
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handleChange} /><br/><br />
+        </div>
     )
 }
 
